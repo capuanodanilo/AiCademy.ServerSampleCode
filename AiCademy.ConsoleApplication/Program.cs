5742e90c23e8637a42e60ac804cd704e1460b774
Danilo Capuano <Admin@NB473.agic.local>
2019-05-06 23:06:49 +0200

    Add project files.

---
+++ AiCademy.ConsoleApplication/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using System.Configuration; 
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+         
+            string username = ConfigurationManager.AppSettings["username"];
+            string password = ConfigurationManager.AppSettings["password"];
+            string crmurl = ConfigurationManager.AppSettings["crmurl"];
+
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            CrmServiceClient conn = new CrmServiceClient("authtype=Office365;Url=" + crmurl + "; Username=" + username + "; Password=" + password);
+            IOrganizationService service = conn.OrganizationServiceProxy;
+
+            Entity contact = new Entity("contact");
+            contact["fullname"] = "Danilo Capuano";
+            contact["firstname"] = "Danilo";
+            contact["lastname"] = "Capuano";
+
+            service.Create(contact);
+
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+}
+
